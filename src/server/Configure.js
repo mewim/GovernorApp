6780e5eb61de896c27d10301096fc5236afe37ed
Chang Liu <liuc223@gmail.com>
2021-12-07 15:59:38 -0500

    Add HTTP proxy  capability

--- src/server/Configure.js
+++ src/server/Configure.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const api = require("./API");
 const path = require("path");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(express.json());
-  app.use("/api", api);
-  const filesPath = path.join(__dirname, "..", "..", "data", "files");
-  app.use("/files", express.static(filesPath));
+  if (process.env.PROXY_API_REQUESTS) {
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "http://waterloo.mew.im:8000/",
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      "/files",
+      createProxyMiddleware({
+        target: "http://waterloo.mew.im:8000/",
+        changeOrigin: true,
+      })
+    );
+  } else {
+    app.use("/api", api);
+    const filesPath = path.join(__dirname, "..", "..", "data", "files");
+    app.use("/files", express.static(filesPath));
+  }
 };
