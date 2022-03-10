5c643bed7c6a425493601b92432c4694dcc0587d
Chang Liu <liuc223@gmail.com>
2022-03-09 19:01:14 -0500

    Add parquet caching capability

--- src/server/Parquet.js
+++ src/server/Parquet.js
@@ -4,8 +4,16 @@ const mongoUtil = require("./MongoUtil");
 const path = require("path");
 const { spawn } = require("child_process");
 const COLLECTION = "inferredstats";
+const fs = require("fs");
 
 const CSV_BASE_PATH = path.join(__dirname, "..", "..", "data", "files");
+const PARQUET_CACHE_PATH = path.join(
+  __dirname,
+  "..",
+  "..",
+  "data",
+  "parquet_cache"
+);
 const PYTHON_PARQUET_CONVERTER_PATH = path.join(
   __dirname,
   "..",
@@ -22,20 +30,52 @@ router.get("/:uuid.parquet", async (req, res) => {
   if (!found) {
     return res.sendStatus(404);
   }
+
+  const useNumberIndex = !(
+    !req.query.use_number_index ||
+    req.query.use_number_index === "0" ||
+    req.query.use_number_index.toLowerCase() === "false"
+  );
+
+  res.setHeader("content-type", "application/octet-stream");
+  res.set("cache-control", "public, max-age=31536000");
+
+  const fileName = `${uuid}${useNumberIndex ? "_num_index" : ""}.parquet`;
+  const filePath = path.join(PARQUET_CACHE_PATH, fileName);
+  const isFileExists = await new Promise((resolve) => {
+    fs.access(filePath, fs.F_OK, (err) => {
+      if (err) {
+        return resolve(false);
+      }
+      return resolve(true);
+    });
+  });
+  if (isFileExists) {
+    return res.sendFile(filePath);
+  }
+
+  const fieldNames = found.schema.fields.map((f) => f.name);
   const encoding = found.encoding;
   const header = found.header;
-  const parquetConverter = spawn("python3", [
+
+  const params = [
     PYTHON_PARQUET_CONVERTER_PATH,
     path.join(CSV_BASE_PATH, `${uuid}.csv`),
     encoding,
     header,
-  ]);
+  ];
+  if (!useNumberIndex) {
+    params.push(JSON.stringify(fieldNames));
+  }
+
+  const parquetConverter = spawn("python3", params);
+  parquetConverter.stderr.pipe(process.stdout);
+
   parquetConverter.stdin.on("error", () => {
     // ignore error on SIGKILL of the Python process so we can quit early
     return;
   });
-  res.setHeader("content-type", "application/octet-stream");
-  res.set('cache-control', 'public, max-age=31536000')
+
   return parquetConverter.stdout.pipe(res);
 });
 
--- utils/CSVToParquetConverter.py
+++ utils/CSVToParquetConverter.py
@@ -2,20 +2,40 @@ import sys
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import os
+import json
+
+CACHE_DIR = os.path.abspath(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "..", "data/parquet_cache/"))
 
 chunksize = 10000
 file_path = sys.argv[1]
 encoding = sys.argv[2]
 header = int(sys.argv[3])
+try:
+    field_names = json.loads(sys.argv[4])
+except:
+    field_names = None
+uuid = os.path.splitext(os.path.basename(file_path))[0]
 
-
-csv_stream = pd.read_csv(file_path, encoding=encoding, chunksize=chunksize, on_bad_lines='skip', dtype=str)
+csv_stream = pd.read_csv(file_path, encoding=encoding,
+                         chunksize=chunksize, on_bad_lines='skip', dtype=str)
 for i, chunk in enumerate(csv_stream):
     chunk.fillna('', inplace=True)
+    if field_names is not None and len(chunk.columns) == len(field_names):
+        chunk.columns = field_names
+    else:
+        chunk.columns = [str(i) for i in range(len(chunk.columns))]
     if i == 0:
+        cache_file_name = "%s%s.parquet" % (
+            uuid, "_num_index" if field_names is None else "")
         parquet_schema = pa.Table.from_pandas(df=chunk).schema
-        parquet_writer = pq.ParquetWriter(sys.stdout.buffer, parquet_schema, compression='snappy')
+        parquet_writer = pq.ParquetWriter(
+            sys.stdout.buffer, parquet_schema, compression='snappy')
+        parquet_cache_writer = pq.ParquetWriter(
+            os.path.join(CACHE_DIR, cache_file_name), parquet_schema, compression='snappy')
 
     table = pa.Table.from_pandas(chunk, schema=parquet_schema)
     parquet_writer.write_table(table)
+    parquet_cache_writer.write_table(table)
 parquet_writer.close()
